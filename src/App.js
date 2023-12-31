10e0adf5023623db10249a65af87195536cf9e3a
ExizMan <yurasankin@yandex.ru>
2023-12-31 12:01:42 +0300

    123

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './img/trace1.svg';
 import './App.css';
 
 function App() {
@@ -7,16 +7,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          C НОВЫМ ГОДОМ
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+         
+
       </header>
     </div>
   );
